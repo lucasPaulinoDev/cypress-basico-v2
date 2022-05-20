abcb42cc10684c7ff032456f71c1560745be7538
Lucas Paulino <lucas@wapstore.com.br>
2022-05-20 16:16:05 -0300

    Cria projeto cypress

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,15 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+describe('Teste', () => {
+
+  it('Testado', () => {
+    cy.visit('https://www.google.com/');
+  });
+
+});
